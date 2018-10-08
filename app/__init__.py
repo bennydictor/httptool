b0672923bc968aef0e9061f169178baa3db15665
Michael Sorokin <benny@bennydictor.tk>
2018-10-08 13:55:30 +0300

    Add home page and docs page stub

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,8 @@
+from . import home, docs
+
 from flask import Flask
 
+
 application = Flask(__name__)
+application.register_blueprint(home.bp)
+application.register_blueprint(docs.bp, url_prefix='/docs')
